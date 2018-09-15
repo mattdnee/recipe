dcb304d626903e9adb33d6a42e7c91a0194bfe17
Matt Nee <mattdnee@gmail.com>
2018-09-15 18:31:47 -0400

    Add ingredient and associated references

--- recipe-web/src/main/java/com/neesolutions/recipe/domain/Recipe.java
+++ recipe-web/src/main/java/com/neesolutions/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.neesolutions.recipe.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -16,6 +17,10 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
     //todo add
     //private Difficulty difficulty;
 
