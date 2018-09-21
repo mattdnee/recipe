04297b7c14ac526db2c6bd456c7ca75d2d358992
Matt Nee <mattdnee@gmail.com>
2018-09-20 22:59:09 -0400

    Add lombok

--- recipe-web/src/main/java/com/neesolutions/recipe/controllers/indexController.java
+++ recipe-web/src/main/java/com/neesolutions/recipe/controllers/indexController.java
@@ -1,33 +1,26 @@
 package com.neesolutions.recipe.controllers;
 
-import com.neesolutions.recipe.domain.Category;
-import com.neesolutions.recipe.domain.UnitOfMeasure;
-import com.neesolutions.recipe.repositories.CategoryRepository;
-import com.neesolutions.recipe.repositories.UnitOfMeasureRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.neesolutions.recipe.domain.Recipe;
+import com.neesolutions.recipe.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.swing.text.html.Option;
-import java.util.Optional;
-
 @Controller
-public class indexController {
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+public class IndexController {
 
-    public indexController(
-            @Autowired CategoryRepository categoryRepository,
-            @Autowired UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
-    @RequestMapping({"", "/", "index.html", "/index.html", "index", "/index"})
-    public String index(){
-        Optional<Category> category = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
-        System.out.println("Cat id: "+category.get().getId()+" uom id: "+unitOfMeasure.get().getId());
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
+
